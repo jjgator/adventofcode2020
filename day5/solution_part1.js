2a6fb78a6656f5ba6724f9e24a93e3fbba483e93
Julie Johnson <julie@shipwell.com>
2020-12-05 14:54:01 -0600

    day 5 input + solution part 1'

---
+++ day5/solution_part1.js
@@ -0,0 +1,32 @@
+import teststring from './input';
+
+const boardingPasses = teststring.split('\n');
+
+const getIndex = (code, limit, upperIndicator, lowerIndicator) => {
+  let lowerLimit = 0;
+  let upperLimit = limit;
+
+  for (let i = 0; i < code.length - 1; i++) {
+    if (code[i] === upperIndicator) {
+      lowerLimit = Math.ceil((lowerLimit + upperLimit) / 2);
+    } else if (code[i] === lowerIndicator) {
+      upperLimit = Math.floor((lowerLimit + upperLimit) / 2);
+    }
+  }
+
+  return code[code.length - 1] === upperIndicator ? upperLimit : lowerLimit;
+};
+
+const findSeatId = (boardingPass) => {
+  const passcode = boardingPass.split('');
+  const rowCode = passcode.slice(0, 7);
+  const columnCode = passcode.slice(7);
+
+  return getIndex(rowCode, 127, 'B', 'F') * 8 + getIndex(columnCode, 7, 'R', 'L');
+};
+
+const seatIDs = boardingPasses.map((bp) => {
+  return findSeatId(bp);
+});
+
+console.log(Math.max(...seatIDs));
